2d66c2a1643e9eff7a2a9640e86a78856128fff8
shakilmahammud <hshakil987@gmail.com>
2021-08-07 18:06:01 +0600

    Day-1 chellege

---
+++ .dist/Day_1.js
@@ -0,0 +1,15 @@
+
+//For inputArray = [1, 2, 1], elementToReplace = 1 and subtractionElem = 3, 
+//the output should be arrayReplace(inputArray, elementToReplace, substrationElem) = [3, 2, 3].
+
+function arrayReplace(inputArray,elementToReplace,substrationElem){
+    inputArray.forEach((element,index)=>{
+      if(element===elementToReplace){
+        inputArray[index]=substrationElem
+      }
+    })
+  console.log(inputArray)
+  }
+  arrayReplace([1,2,1],1,3)
+  
+  
---
+++ README.md
@@ -0,0 +1,1 @@
+# 100_algorithm_chellenge
