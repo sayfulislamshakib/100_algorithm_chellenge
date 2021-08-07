c4f7d00771c817df949b90c6100bd079dabe84c2
Sayful Islam Shakib <connect.sayful@gmail.com>
2021-08-07 18:50:36 +0600

    Update Day_1.js

--- Day_1.js
+++ Day_1.js
@@ -3,13 +3,13 @@
 //the output should be arrayReplace(inputArray, elementToReplace, substrationElem) = [3, 2, 3].
 
 function arrayReplace(inputArray,elementToReplace,substrationElem){
-    inputArray.forEach((element,index)=>{
-      if(element===elementToReplace){
-        inputArray[index]=substrationElem
+    inputArray.forEach((element,index) =>{
+      if(element === elementToReplace){
+        inputArray[ index ] = substrationElem
       }
     })
-  console.log(inputArray)
+  console.log( inputArray )
   }
-  arrayReplace([1,2,1],1,3)
+  arrayReplace( [1,2,1],1,3 )
   
   
